08873dc6221a4adcba9ec81a97a8d567588b0028
lukaeric14 <lukaericnu@gmail.com>
2022-09-27 09:35:31 +0530

    comments api work

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -1,19 +1,20 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
-const withAuth = require('../../utils/authentication');
+const router = require("express").Router();
+const { Comments } = require("../../models");
+const withAuth = require("../../utils/authentication");
 
-router.post('/', withAuth, async (req, res) => {
-    Comment.create({
-        comment_content: req.body.commentContent,
-        post_id: req.body.postId,
-        user_id: req.session.user_id,
-      })
+router.post("/", withAuth, async (req, res) => {
+  console.log(req.body);
+  Comments.create({
+    comment: req.body.comment,
+    post_id: req.body.post_id,
+    user_id: req.session.userId,
+  })
 
-        .then((cmmnt) => res.json(cmmnt))
-    
-        .catch((error) => {
-          res.status(400).json(error);
-        });
+    .then((cmmnt) => res.json(cmmnt))
+
+    .catch((error) => {
+      res.status(400).json(error);
+    });
 });
 
 module.exports = router;
