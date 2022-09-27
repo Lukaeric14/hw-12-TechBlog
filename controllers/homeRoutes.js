01153ad436f36b8f704698c1dc96462bf8a246f8
lukaeric14 <lukaericnu@gmail.com>
2022-09-27 09:36:18 +0530

    login for all pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,80 +1,85 @@
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const router = require('express').Router();
+const sequelize = require("../config/connection");
+const { Posts, User, Comments } = require("../models");
+const router = require("express").Router();
 
-router.get('/', async (req, res) => {
-    try {
-        // Get all posts and comments
-        const postData = await Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['username'],
-                },
-            ]
-        });
+router.get("/", async (req, res) => {
+  try {
+    console.log("Getting all routes");
+    // Get all posts and comments
+    const postData = await Posts.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
 
-        // Gets data from posts
-        const posts = postData.map((post) => post.get({ plain: true }));
+    // Gets data from posts
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-        // Passes posts and session data into mustache
-        res.render('all-posts', {
-            layout: 'main',
-            posts,
-            logged_in: req.session.loggedIn
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    // Passes posts and session data into mustache
+    res.render("home", {
+      layout: "main",
+      posts,
+      logged_in: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // get single post
-router.get('/post/:id', async (req, res) => {
-    try {
-        const postData = await Post.findByPk(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['username'],
-                },
-                {
-                    model: Comment,
-                    attributes: ['body'],
-                },
-            ],
-        });
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Posts.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comments,
+          attributes: ["comment"],
+        },
+      ],
+    });
 
-        // Gets data from single post
-        const post = postData.get({ plain: true });
+    // Gets data from single post
+    const post = postData.get({ plain: true });
 
-        // Passes post and session status to mustache
-        res.render('single-post', {
-            ...post,
-            logged_in: req.session.loggedIn
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    console.log(post);
+
+    // Passes post and session status to mustache
+    res.render("single-post", {
+      ...post,
+      logged_in: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Redirects to homepage after login
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-    res.render('login');
+  res.render("login");
 });
 
 // Redirects to homepage after sign up
-router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-    res.render('signup');
+  res.render("signup");
 });
 
 module.exports = router;
--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,52 +1,55 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/authentication');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Posts, User, Comments } = require("../models");
+const withAuth = require("../utils/authentication");
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            where: {
-                userId: req.session.userId,
-            },
-        });
+router.get("/", withAuth, async (req, res) => {
+  try {
+    console.log("opening dashboard");
+    const postData = await Posts.findAll({
+      where: {
+        user_id: req.session.userId,
+      },
+    });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
 
-        res.render('all-posts-admin', {
-            layout: 'dashboard',
-            posts,
-        });
-    } catch (err) {
-        res.redirect('login');
-    }
+    res.render("all-posts-admin", {
+      layout: "main",
+      posts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect("login");
+  }
 });
 
 // Enter a new post
-router.get('/new', withAuth, (req, res) => {
-    res.render('new-post', {
-        layout: 'dashboard',
-    });
+router.get("/new", withAuth, (req, res) => {
+  res.render("new-post", {
+    layout: "main",
+  });
 });
 
 // Edit a specific post
-router.get('/edit/:id', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.findByPk(req.params.id);
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Posts.findByPk(req.params.id);
 
-        if (postData) {
-            const post = postData.get({ plain: true });
+    if (postData) {
+      const post = postData.get({ plain: true });
 
-            res.render('edit-post', {
-                layout: 'dashboard',
-                post,
-            });
-        } else {
-            res.status(404).end();
-        }
-    } catch (err) {
-        res.redirect('login');
+      res.render("edit-post", {
+        layout: "dashboard",
+        post,
+      });
+    } else {
+      res.status(404).end();
     }
+  } catch (err) {
+    res.redirect("login");
+  }
 });
 
 module.exports = router;
